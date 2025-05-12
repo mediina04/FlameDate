4c9533bd092de4ef38418e1e553ae8b52e5fae2e
DarthRueda <ruedaar04@gmail.com>
2025-05-12 17:03:08 +0200

    Commit defectuoso

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = 'Asdqwe!23'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flamedate_user:Asdqwe!23@localhost/flamedate'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/flamedate'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
